954468575653df0cdaa87e54ff2dde6a52477d0a
ashikulashi <ashismart554@gmail.com>
2022-11-17 13:23:40 +0530

    Ashik done his coding

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -2,8 +2,15 @@ package org.sample;
 
 public class Sample {
 	
+	
+	//Master Coding
 	private void code1() {
 		System.out.println("Code1");
+
+	}
+	//Ashik Coding
+	private void code2() {
+		System.out.println("Code2");
 
 	}
 
